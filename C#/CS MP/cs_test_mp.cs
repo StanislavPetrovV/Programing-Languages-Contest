db37e400d35c07f7e52891f9ce8730a7e0c8cefb
StanislavPetrovV <34447380+StanislavPetrovV@users.noreply.github.com>
2020-11-23 23:54:38 +0800

    C# MP

---
+++ C#/CS MP/cs_test_mp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static bool IsPrime(int num)
+    {
+        if (num == 2) return true;
+        if (num <= 1 || num % 2 == 0) return false;
+
+        double sqrt_num = Math.Sqrt(num);
+        for (int div = 3;  div <= sqrt_num; div += 2)
+        {
+            if (num % div == 0) return false;
+        }
+        
+        return true;
+        
+    }
+
+    public static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        int N = 10000000;
+        stopwatch.Start();
+        Parallel.For(0, N, new ParallelOptions { MaxDegreeOfParallelism = 4 }, index =>  
+        { 
+            IsPrime(index);
+        });
+            
+        
+        stopwatch.Stop();
+        Console.WriteLine(stopwatch.Elapsed.TotalSeconds);
+    }
+}
+
+
