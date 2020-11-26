aed38a87d6b944ace3b9b36c9b84979fe64b84bd
RemiIia <valent.xarin@gmail.com>
2020-11-26 17:31:48 +0300

    Update Rust implementation

---
+++ Rust/benches/my_benchmark.rs
@@ -0,0 +1,35 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rayon::prelude::*;
+
+const N: i32 = 10000000;
+
+fn is_prime(num: f32) -> bool {
+    if num == 2. {
+        true
+    } else if num <= 1. || (num % 2.) == 0. {
+        false
+    } else {
+        // let to = (num as f32).sqrt() as i32;
+        let mut div = 3.;
+        while div <= num.sqrt() {
+            if (num % div) == 0. {
+                return false;
+            }
+            div += 2.;
+        }
+        true
+    }
+}
+
+fn is_prime_bench(c: &mut Criterion) {
+    c.bench_function("is_prime 10000000", |b| {
+        b.iter(|| {
+            (0..N).into_par_iter().for_each(|x| {
+                is_prime(x as f32);
+            })
+        })
+    });
+}
+
+criterion_group!(benches, is_prime_bench);
+criterion_main!(benches);
